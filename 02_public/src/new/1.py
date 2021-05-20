64d4f8dbdf1aa44e8be18e664babe267844d2bca
billbuchanan <w.buchanan@napier.ac.uk>
2021-05-20 12:05:52 +0100

    Update

---
+++ 02_public/src/new/1.py
@@ -0,0 +1,16 @@
+# Python 2.7
+p=11
+q=3
+N=p*q
+PHI=(p-1)*(q-1)
+e=3
+for d in range(1,100):
+        if ((e*d % PHI)==1): break
+print (e,N)
+print (d,N)
+M=4
+cipher = M**e % N
+print ("Cipher is: ",cipher)
+message = cipher**d % N
+print (message)
+
